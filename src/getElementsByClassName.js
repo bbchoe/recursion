95782063012304e5341a7630f27450ee26014a6b
Brian Choe <brian@visibook.com>
2017-10-04 19:06:29 -0700

    finished all required recursion materials

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // get input classname
+  // return all document elements that possess this class name
+  // return in the form of a flat array
+  var outputArray = [];
+
+  getMatchingNodes(document.body);
+
+  function getMatchingNodes(inputObj) {
+      if (inputObj.classList !== undefined && inputObj.classList.contains(className)) {
+          outputArray.push(inputObj);
+      }
+      if (inputObj.hasChildNodes()) {
+          var children = inputObj.childNodes;
+          for (var i = 0; i < children.length; i++) {
+              getMatchingNodes(children[i]);
+          }
+      }
+  }
+  return outputArray;
 };
